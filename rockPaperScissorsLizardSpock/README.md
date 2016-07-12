eeb895b0f55adb7aea00c0263cfc01002326e48a
Eric Lee Carraway <github@ericcarraway.com>
2016-07-11 22:16:45 -0500

    rockPaperScissorsLizardSpock update rules; tests

--- rockPaperScissorsLizardSpock/README.md
+++ rockPaperScissorsLizardSpock/README.md
@@ -4,6 +4,9 @@
 
 _Image by Wikipedia user Nojhan (CC BY-SA)_
 
+In the game of rock-paper-scissors-lizard-Spock, players A and B simultaneously reveal a choice of
+rock, paper, scissors, lizard, or Spock.
+
 * scissors cut paper
 * paper covers rock
 * rock crushes lizard
@@ -15,4 +18,11 @@ _Image by Wikipedia user Nojhan (CC BY-SA)_
 * Spock vaporizes rock
 * rock crushes scissors
 
----
+It is a tie if both players play the same thing.
+
+Write a function `rockPaperScissorsLizardSpock(a, b)`, where `a` and `b` are the player's moves,
+which must be `'rock'` or `'paper'` or `'scissors'` or `'lizard'` or `'Spock'`.
+
+Return `1` if player A wins, `-1` if player B wins, and `0` if it is a tie.
+
+_Adapted from http://codingbat.com/prob/p270750?parent=/home/peter@norvig.com _
--- rockPaperScissorsLizardSpock/test/rockPaperScissorsLizardSpock.spec.js
+++ rockPaperScissorsLizardSpock/test/rockPaperScissorsLizardSpock.spec.js
@@ -11,4 +11,68 @@ describe.only('rockPaperScissorsLizardSpock', function () {
             rockPaperScissors.should.be.a('function');
         });
     });
+
+    describe('ties', function () {
+        it('rock', function () {
+            rockPaperScissors('rock', 'rock').should.equal(0);
+        });
+
+        it('paper', function () {
+            rockPaperScissors('paper', 'paper').should.equal(0);
+        });
+
+        it('scissors', function () {
+            rockPaperScissors('scissors', 'scissors').should.equal(0);
+        });
+
+        it('lizard', function () {
+            rockPaperScissors('lizard', 'lizard').should.equal(0);
+        });
+
+        it('Spock', function () {
+            rockPaperScissors('Spock', 'Spock').should.equal(0);
+        });
+    });
+
+    describe('a winning', function () {
+        it('scissors cut paper', function () {
+            rockPaperScissors('scissors', 'paper').should.equal(1);
+        });
+
+        it('paper covers rock', function () {
+            rockPaperScissors('paper', 'rock').should.equal(1);
+        });
+
+        it('rock crushes lizard', function () {
+            rockPaperScissors('rock', 'lizard').should.equal(1);
+        });
+
+        it('lizard poisons Spock', function () {
+            rockPaperScissors('lizard', 'Spock').should.equal(1);
+        });
+
+        it('Spock smashes scissors', function () {
+            rockPaperScissors('Spock', 'scissors').should.equal(1);
+        });
+
+        it('scissors decapitates lizard', function () {
+            rockPaperScissors('scissors', 'lizard').should.equal(1);
+        });
+
+        it('lizard eats paper', function () {
+            rockPaperScissors('lizard', 'paper').should.equal(1);
+        });
+
+        it('paper disproves Spock', function () {
+            rockPaperScissors('paper', 'Spock').should.equal(1);
+        });
+
+        it('Spock vaporizes rock', function () {
+            rockPaperScissors('Spock', 'rock').should.equal(1);
+        });
+
+        it('rock crushes scissors', function () {
+            rockPaperScissors('rock', 'scissors').should.equal(1);
+        });
+    });
 });
